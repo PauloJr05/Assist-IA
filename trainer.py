ce012a8ca336c7c95fd69acd2c24bae6f0a057ff
PauloJr05 <paulohenriquebonfimjr@gmail.com>
2018-09-09 12:06:35 -0300

    "Arquivo de treinamento"

---
+++ trainer.py
@@ -0,0 +1,17 @@
+#-*- coding: utf-8 -*-
+
+
+from  chatterbot.trainers import ListTrainer
+from  chatterbot import ChatBot
+
+import os
+
+bot = ChatBot('Raquel')
+
+bot.set_trainer(ListTrainer) # define metodo de treinamento
+
+for _file in os.listdir('Chats'): #Percorrer todos os arquivos e chats
+    lines = open('Chats/'+ _file, 'r').readlines() # Ler as linhs dos arquivos dos chats
+
+    bot.train(lines)
+
