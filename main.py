026413dbb8b415ca5b9195e0973265f7076f9af7
PauloJr05 <paulohenriquebonfimjr@gmail.com>
2018-09-09 12:06:14 -0300

    "main teste"

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+from  chatterbot.trainers import ListTrainer
+from  chatterbot import ChatBot
+
+
+
+
+bot = ChatBot('Raquel', read_only = True)
+while True:
+    eu = input('Eu: ')
+    print('Bot:{}'.format(bot.get_response(eu)))
+
+
